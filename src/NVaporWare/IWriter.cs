4d3db5e5bec8a2ece9089799a11da48f15a6cf71
bitterskittles <yigit.meltem@gmail.com>
2013-02-08 17:40:39 +0000

    Added BitmapWriter.

---
+++ src/NVaporWare/IWriter.cs
@@ -0,0 +1,49 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="IWriter.cs" company="bitterskittles">
+//   Copyright © 2013 bitterskittles.
+//   This program is free software. It comes without any warranty, to
+//   the extent permitted by applicable law. You can redistribute it
+//   and/or modify it under the terms of the Do What The Fuck You Want
+//   To Public License, Version 2, as published by Sam Hocevar. See
+//   http://www.wtfpl.net/ for more details.
+// </copyright>
+// <summary>
+//   Defines the IWriter type.
+// </summary>
+// --------------------------------------------------------------------------------------------------------------------
+
+namespace NVaporWare
+{
+    using System;
+    using System.Diagnostics.Contracts;
+    using System.IO;
+
+    [ContractClass(typeof(IWriterContract<>))]
+    public interface IWriter<in T>
+    {
+        #region Public Methods and Operators
+
+        void Write(Stream stream, int offset, T bitmap);
+
+        #endregion
+    }
+
+    [ContractClassFor(typeof(IWriter<>))]
+    public abstract class IWriterContract<T> : IWriter<T>
+    {
+        #region Public Methods and Operators
+
+        public void Write(Stream stream, int offset, T bitmap)
+        {
+            Contract.Requires<ArgumentNullException>(stream != null);
+            Contract.Requires<ArgumentException>(stream.CanSeek);
+            Contract.Requires<ArgumentException>(stream.CanWrite);
+            Contract.Requires<ArgumentOutOfRangeException>(offset >= 0);
+            Contract.Requires<ArgumentOutOfRangeException>(offset <= 0xFFFF);
+
+            throw new NotImplementedException();
+        }
+
+        #endregion
+    }
+}
---
+++ test/NVaporWare.Test/BitmapWriterTests.cs
@@ -0,0 +1,55 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="BitmapWriterTests.cs" company="bitterskittles">
+//   Copyright © 2013 bitterskittles.
+//   This program is free software. It comes without any warranty, to
+//   the extent permitted by applicable law. You can redistribute it
+//   and/or modify it under the terms of the Do What The Fuck You Want
+//   To Public License, Version 2, as published by Sam Hocevar. See
+//   http://www.wtfpl.net/ for more details.
+// </copyright>
+// <summary>
+//   Defines the BitmapWriterTests type.
+// </summary>
+// --------------------------------------------------------------------------------------------------------------------
+
+namespace NVaporWare.Test
+{
+    using System.Drawing;
+    using System.IO;
+
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+    [TestClass]
+    public class BitmapWriterTests
+    {
+        #region Public Methods and Operators
+
+        [TestMethod]
+        [DeploymentItem(".\\TestData\\Evic11.dec")]
+        [DeploymentItem(".\\TestData\\Evic11_0x2FC1.bmp")]
+        public void WriteTest()
+        {
+            var bitmap = (Bitmap)Image.FromFile(".\\Evic11_0x2FC1.bmp");
+
+            byte[] actual = null;
+            byte[] expected = null;
+            using (var fileStream = new FileStream("Evic11.dec", FileMode.Open, FileAccess.Read))
+            using (var memoryStream = new MemoryStream(new byte[fileStream.Length]))
+            {
+                fileStream.CopyTo(memoryStream);
+
+                var bitmapWriter = new BitmapWriter();
+                bitmapWriter.Write(memoryStream, 0x2FC1, bitmap);
+                actual = memoryStream.ToArray();
+
+                expected = new byte[fileStream.Length];
+                fileStream.Seek(0, SeekOrigin.Begin);
+                fileStream.Read(expected, 0, expected.Length);
+            }
+
+            CollectionAssert.AreEquivalent(expected, actual);
+        }
+
+        #endregion
+    }
+}
